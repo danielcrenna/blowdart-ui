7f5d16c155597fd18820ec75dc122296a9d027e0
Daniel Crenna <daniel.crenna@gmail.com>
2019-11-11 01:20:31 -0500

    minor cleanup

--- src/Demo.Examples/Pages/ElementsPage.cs
+++ src/Demo.Examples/Pages/ElementsPage.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Daniel Crenna & Contributors. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using Blowdart.UI;
-using Blowdart.UI.Patterns;
 
 namespace Demo.Examples.Pages
 {
