b0ac6eb70e85e5f0c73fbe6b2962c6c990f50529
Daniel Crenna <daniel.crenna@gmail.com>
2020-05-01 12:32:11 -0400

    replace h1 and literal on Index page

---
+++ test/Demo/DemoPages.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Blowdart.UI;
+
+namespace Demo
+{
+	public static class DemoPages
+	{
+		public static void Index(Ui ui)
+		{
+			ui.BeginElement("h1");
+			ui.Text("Hello, world!");
+			ui.EndElement("h1");
+
+			ui.Text("Welcome to your new app.");
+		}
+	}
+}
--- test/Demo/Program.cs
+++ test/Demo/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Demo
 {
-    public class Program
+	public class Program
     {
         public static async Task Main(string[] args)
         {
